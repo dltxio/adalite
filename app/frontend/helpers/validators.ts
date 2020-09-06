812fab4751f2bc693a24cd88533b1c245b6fcaf8
Peter Benc <peter.benc1@gmail.com>
2020-09-06 21:55:14 +0200

    wip 3

--- app/frontend/helpers/validators.ts
+++ app/frontend/helpers/validators.ts
@@ -1,4 +1,4 @@
-import {isValidByronAddress, isValidShelleyAddress} from 'cardano-crypto.js'
+import {isValidBootstrapAddress, isValidShelleyAddress} from 'cardano-crypto.js'
 import {ADALITE_CONFIG} from '../config'
 import {toCoins} from './adaConverters'
 import {validateMnemonic} from '../wallet/mnemonic'
@@ -8,8 +8,8 @@ const {ADALITE_MIN_DONATION_VALUE} = ADALITE_CONFIG
 const parseToLovelace = (str): Lovelace => Math.trunc(toCoins(parseFloat(str) as Ada)) as Lovelace
 
 const _sendAddressValidators = {
-  byron: isValidByronAddress,
-  shelley: (address) => isValidShelleyAddress(address) || isValidByronAddress(address),
+  byron: isValidBootstrapAddress,
+  shelley: (address) => isValidShelleyAddress(address) || isValidBootstrapAddress(address),
 }
 
 const sendAddressValidator = (fieldValue) =>
